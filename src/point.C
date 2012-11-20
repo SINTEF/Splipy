33a2ceb81c1f419eb388f23323f79d5be2dea410
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2012-11-20 09:29:11 +0100

    fix: why do i always push without compile testing first..

--- src/point.C
+++ src/point.C
@@ -179,7 +179,7 @@ PyObject* Point_Div(PyObject* o1, PyObject* o2)
   if (PyObject_TypeCheck(o2,&PyFloat_Type)) {
     result = (Point*)Point_Type.tp_alloc(&Point_Type,0);
     result->data.reset(new Go::Point(*p1/PyFloat_AsDouble(o2)));
-  } else if (PyObject_TypeCheck(o2r,&PyInt_Type)) {
+  } else if (PyObject_TypeCheck(o2,&PyInt_Type)) {
     result = (Point*)Point_Type.tp_alloc(&Point_Type,0);
     result->data.reset(new Go::Point(*p1/PyInt_AsLong(o2)));
   }
