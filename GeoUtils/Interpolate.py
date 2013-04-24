5c775b13f8a03edd5c8016e9f624560fca7f0dc5
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2013-04-24 15:36:55 +0200

    changed: replace tabs with spaces
    tabs are evil, and with python they are an outright problem as you
    cannot mix them with spaces. stick to spaces!

--- GeoUtils/Interpolate.py
+++ GeoUtils/Interpolate.py
@@ -4,124 +4,124 @@ import GoTools
 import GoTools.CurveFactory 
 
 def Linear(x,y=[],z=[]):
-	"""Linear interpolate a list of points (arclength parametrization) in an approximative sense
-	@param x: The x-coordinate of the points to interpolate
-	@type x: List of floats
-	@param y: y-coordinates
-	@type y: List of floats
-	@param z: z-coordinates
-	@type z: List of floats
-	@return: Linear interpolated curve in 3 dimensions
-	@rtype: Curve
-	"""
-	if(len(y) == 0):
-		for i in range(len(x)):
-			y.append(0)
-	if(len(z) == 0):
-		for i in range(len(x)):
-			z.append(0)
-	if(len(x) != len(y) or len(x) != len(y) or len(y) != len(z)):
-		print "Error: coordinate lists needs to be of same length"
-		return None 
+        """Linear interpolate a list of points (arclength parametrization) in an approximative sense
+        @param x: The x-coordinate of the points to interpolate
+        @type x: List of floats
+        @param y: y-coordinates
+        @type y: List of floats
+        @param z: z-coordinates
+        @type z: List of floats
+        @return: Linear interpolated curve in 3 dimensions
+        @rtype: Curve
+        """
+        if(len(y) == 0):
+                for i in range(len(x)):
+                        y.append(0)
+        if(len(z) == 0):
+                for i in range(len(x)):
+                        z.append(0)
+        if(len(x) != len(y) or len(x) != len(y) or len(y) != len(z)):
+                print "Error: coordinate lists needs to be of same length"
+                return None 
 
-	N = len(x)
-	pts = []
-	t   = [0]
-	for i in range(N):
-		pts.append(GoTools.Point(x[i],y[i],z[i]))
-		if(i>0):
-			t.append(t[i-1] + abs(pts[i]-pts[i-1])) # eucledian distance between points i and (i-1)
-	return GoTools.CurveFactory.ApproximateCurve(pts, t, 2)
+        N = len(x)
+        pts = []
+        t   = [0]
+        for i in range(N):
+                pts.append(GoTools.Point(x[i],y[i],z[i]))
+                if(i>0):
+                        t.append(t[i-1] + abs(pts[i]-pts[i-1])) # eucledian distance between points i and (i-1)
+        return GoTools.CurveFactory.ApproximateCurve(pts, t, 2)
 
 def LinearP(pts):
-	"""Linear interpolate a list of points (arclength parametrization) in an approximative sense
-	@param pts: The coordinate of the points to interpolate
-	@type pts: List of Points
-	@return: Linear interpolated curve
-	@rtype: Curve
-	"""
-	N = len(pts)
-	t   = [0]
-	for i in range(1,N):
-		t.append(t[i-1] + abs(pts[i]-pts[i-1])) # eucledian distance between points i and (i-1)
-	return GoTools.CurveFactory.ApproximateCurve(pts, t, 2)
+        """Linear interpolate a list of points (arclength parametrization) in an approximative sense
+        @param pts: The coordinate of the points to interpolate
+        @type pts: List of Points
+        @return: Linear interpolated curve
+        @rtype: Curve
+        """
+        N = len(pts)
+        t   = [0]
+        for i in range(1,N):
+                t.append(t[i-1] + abs(pts[i]-pts[i-1])) # eucledian distance between points i and (i-1)
+        return GoTools.CurveFactory.ApproximateCurve(pts, t, 2)
 
 def Cubic(x, y=[], z=[]):
-	"""Cubic spline interpolation a list of points by arclength parametrization  in an approximative sense
-	@param x: The x-coordinate of the points to interpolate
-	@type x: List of floats
-	@param y: y-coordinates
-	@type y: List of floats
-	@param z: z-coordinates
-	@type z: List of floats
-	@return: Cubic interpolated curve in 3 dimensions
-	@rtype: Curve
-	"""
-	if(len(y) == 0):
-		for i in range(len(x)):
-			y.append(0)
-	if(len(z) == 0):
-		for i in range(len(x)):
-			z.append(0)
-	if(len(x) != len(y) or len(x) != len(y) or len(y) != len(z)):
-		print "Error: coordinate lists needs to be of same length"
-		return None 
+        """Cubic spline interpolation a list of points by arclength parametrization  in an approximative sense
+        @param x: The x-coordinate of the points to interpolate
+        @type x: List of floats
+        @param y: y-coordinates
+        @type y: List of floats
+        @param z: z-coordinates
+        @type z: List of floats
+        @return: Cubic interpolated curve in 3 dimensions
+        @rtype: Curve
+        """
+        if(len(y) == 0):
+                for i in range(len(x)):
+                        y.append(0)
+        if(len(z) == 0):
+                for i in range(len(x)):
+                        z.append(0)
+        if(len(x) != len(y) or len(x) != len(y) or len(y) != len(z)):
+                print "Error: coordinate lists needs to be of same length"
+                return None 
 
-	N   = len(x)
-	pts = []
-	t   = [0]
-	for i in range(N):
-		pts.append(GoTools.Point(x[i],y[i],z[i]))
-		if(i>0):
-			t.append(t[i-1] + abs(pts[i]-pts[i-1])) # eucledian distance between points i and (i-1)
-	return GoTools.CurveFactory.ApproximateCurve(pts, t, 4)
+        N   = len(x)
+        pts = []
+        t   = [0]
+        for i in range(N):
+                pts.append(GoTools.Point(x[i],y[i],z[i]))
+                if(i>0):
+                        t.append(t[i-1] + abs(pts[i]-pts[i-1])) # eucledian distance between points i and (i-1)
+        return GoTools.CurveFactory.ApproximateCurve(pts, t, 4)
 
 def CubicP(pts):
-	"""Cubic spline interpolation a list of points by arclength parametrization in an approximative sense
-	@param pts: The coordinate of the points to interpolate
-	@type pts: List of Points
-	@return: Cubic interpolated curve 
-	@rtype: Curve
-	"""
-	N = len(pts)
-	t   = [0]
-	for i in range(1,N):
-		t.append(t[i-1] + abs(pts[i]-pts[i-1])) # eucledian distance between points i and (i-1)
-	return GoTools.CurveFactory.ApproximateCurve(pts, t, 4)
-	
+        """Cubic spline interpolation a list of points by arclength parametrization in an approximative sense
+        @param pts: The coordinate of the points to interpolate
+        @type pts: List of Points
+        @return: Cubic interpolated curve 
+        @rtype: Curve
+        """
+        N = len(pts)
+        t   = [0]
+        for i in range(1,N):
+                t.append(t[i-1] + abs(pts[i]-pts[i-1])) # eucledian distance between points i and (i-1)
+        return GoTools.CurveFactory.ApproximateCurve(pts, t, 4)
+        
 def UniformCubic(x, y=[], z=[]):
-	"""Cubic spline interpolation a list of points by arclength parametrization
-	@param x: The x-coordinate of the points to interpolate
-	@type x: List of floats
-	@param y: y-coordinates
-	@type y: List of floats
-	@param z: z-coordinates
-	@type z: List of floats
-	@return: Cubic interpolated curve in 3 dimensions
-	@rtype: Curve
-	"""
-	if(len(y) == 0):
-		for i in range(len(x)):
-			y.append(0)
-	if(len(z) == 0):
-		for i in range(len(x)):
-			z.append(0)
-	if(len(x) != len(y) or len(x) != len(y) or len(y) != len(z)):
-		print "Error: coordinate lists needs to be of same length"
-		return None 
+        """Cubic spline interpolation a list of points by arclength parametrization
+        @param x: The x-coordinate of the points to interpolate
+        @type x: List of floats
+        @param y: y-coordinates
+        @type y: List of floats
+        @param z: z-coordinates
+        @type z: List of floats
+        @return: Cubic interpolated curve in 3 dimensions
+        @rtype: Curve
+        """
+        if(len(y) == 0):
+                for i in range(len(x)):
+                        y.append(0)
+        if(len(z) == 0):
+                for i in range(len(x)):
+                        z.append(0)
+        if(len(x) != len(y) or len(x) != len(y) or len(y) != len(z)):
+                print "Error: coordinate lists needs to be of same length"
+                return None 
 
-	N   = len(x)
-	pts = []
-	for i in range(N):
-		pts.append(GoTools.Point(x[i],y[i],z[i]))
-	return GoTools.CurveFactory.ApproximateCurve(pts, range(N), 4)
-	
+        N   = len(x)
+        pts = []
+        for i in range(N):
+                pts.append(GoTools.Point(x[i],y[i],z[i]))
+        return GoTools.CurveFactory.ApproximateCurve(pts, range(N), 4)
+        
 def UniformCubicP(pts):
-	"""Cubic spline interpolation a list of points by uniform parametrization
-	@param pts: The coordinate of the points to interpolate
-	@type pts: List of Points
-	@return: Cubic interpolated curve 
-	@rtype: Curve
-	"""
-	return GoTools.CurveFactory.ApproximateCurve(pts, range(len(pts)), 4)
+        """Cubic spline interpolation a list of points by uniform parametrization
+        @param pts: The coordinate of the points to interpolate
+        @type pts: List of Points
+        @return: Cubic interpolated curve 
+        @rtype: Curve
+        """
+        return GoTools.CurveFactory.ApproximateCurve(pts, range(len(pts)), 4)
 
--- GeoUtils/Refinement.py
+++ GeoUtils/Refinement.py
@@ -4,70 +4,70 @@ from math import atan
 from math import pi
 
 def UniformCurve(curve, n=1):
-	"""Uniformly refine a curve by halfing each knot interval
-	@param curve: The curve to refine
-	@type curve: Curve
-	@param n: number of new knots in each interval
-	@type n: Int
-	@return: None
-	"""
-	
-	knots = curve.GetKnots()
-	for i in range(0,len(knots)-1):
-		for j in range(n):
-			newKnot = knots[i] + 1.0*(knots[i+1]-knots[i])/(n+1)*(j+1)
-			curve.InsertKnot(newKnot);
+        """Uniformly refine a curve by halfing each knot interval
+        @param curve: The curve to refine
+        @type curve: Curve
+        @param n: number of new knots in each interval
+        @type n: Int
+        @return: None
+        """
+        
+        knots = curve.GetKnots()
+        for i in range(0,len(knots)-1):
+                for j in range(n):
+                        newKnot = knots[i] + 1.0*(knots[i+1]-knots[i])/(n+1)*(j+1)
+                        curve.InsertKnot(newKnot);
 
 # Chop each knot span in half
 def UniformSurface(surface, direction=0, n=1):
-	"""Uniformly refine a surface by halfing each knot interval
-	@param surface: The surface to refine
-	@type surface: Surface 
-	@param direction: The direction to refine in (0 = both, 1, 2)
-	@type direction: integer
-	@param n: number of new knots in each interval
-	@type n: Int
-	@return: None
-	"""
-	knots_u, knots_v = surface.GetKnots()
-	if direction == 0 or direction == 1:
-		for i in range(0,len(knots_u)-1):
-			for j in range(n):
-				newKnot = knots_u[i] + 1.0*(knots_u[i+1]-knots_u[i])/(n+1)*(j+1)
-				surface.InsertKnot(0, newKnot);
-	if direction == 0 or direction == 2:
-		for i in range(0,len(knots_v)-1):
-			for j in range(n):
-				newKnot = knots_v[i] + 1.0*(knots_v[i+1]-knots_v[i])/(n+1)*(j+1)
-				surface.InsertKnot(1, newKnot);
+        """Uniformly refine a surface by halfing each knot interval
+        @param surface: The surface to refine
+        @type surface: Surface 
+        @param direction: The direction to refine in (0 = both, 1, 2)
+        @type direction: integer
+        @param n: number of new knots in each interval
+        @type n: Int
+        @return: None
+        """
+        knots_u, knots_v = surface.GetKnots()
+        if direction == 0 or direction == 1:
+                for i in range(0,len(knots_u)-1):
+                        for j in range(n):
+                                newKnot = knots_u[i] + 1.0*(knots_u[i+1]-knots_u[i])/(n+1)*(j+1)
+                                surface.InsertKnot(0, newKnot);
+        if direction == 0 or direction == 2:
+                for i in range(0,len(knots_v)-1):
+                        for j in range(n):
+                                newKnot = knots_v[i] + 1.0*(knots_v[i+1]-knots_v[i])/(n+1)*(j+1)
+                                surface.InsertKnot(1, newKnot);
 
 # Chop each knot span in half
 def UniformVolume(volume, direction=0, n=1):
-	"""Uniformly refine a volume by halfing each knot interval
-	@param volume: The volume to refine
-	@type volume: Volume 
-	@param direction: The direction to refine in (0 = both, 1, 2, 3)
-	@type direction: integer
-	@param n: number of new knots in each interval
-	@type n: Int
-	@return: None
-	"""
-	knots_u, knots_v, knots_w = volume.GetKnots()
-	if direction == 0 or direction == 1:
-		for i in range(0,len(knots_u)-1):
-			for j in range(n):
-				newKnot = knots_u[i] + 1.0*(knots_u[i+1]-knots_u[i])/(n+1)*(j+1)
-				volume.InsertKnot(0, newKnot);
-	if direction == 0 or direction == 2:
-		for i in range(0,len(knots_v)-1):
-			for j in range(n):
-				newKnot = knots_v[i] + 1.0*(knots_v[i+1]-knots_v[i])/(n+1)*(j+1)
-				volume.InsertKnot(1, newKnot);
-	if direction == 0 or direction == 3:
-		for i in range(0,len(knots_w)-1):
-			for j in range(n):
-				newKnot = knots_w[i] + 1.0*(knots_w[i+1]-knots_w[i])/(n+1)*(j+1)
-				volume.InsertKnot(2, newKnot);
+        """Uniformly refine a volume by halfing each knot interval
+        @param volume: The volume to refine
+        @type volume: Volume 
+        @param direction: The direction to refine in (0 = both, 1, 2, 3)
+        @type direction: integer
+        @param n: number of new knots in each interval
+        @type n: Int
+        @return: None
+        """
+        knots_u, knots_v, knots_w = volume.GetKnots()
+        if direction == 0 or direction == 1:
+                for i in range(0,len(knots_u)-1):
+                        for j in range(n):
+                                newKnot = knots_u[i] + 1.0*(knots_u[i+1]-knots_u[i])/(n+1)*(j+1)
+                                volume.InsertKnot(0, newKnot);
+        if direction == 0 or direction == 2:
+                for i in range(0,len(knots_v)-1):
+                        for j in range(n):
+                                newKnot = knots_v[i] + 1.0*(knots_v[i+1]-knots_v[i])/(n+1)*(j+1)
+                                volume.InsertKnot(1, newKnot);
+        if direction == 0 or direction == 3:
+                for i in range(0,len(knots_w)-1):
+                        for j in range(n):
+                                newKnot = knots_w[i] + 1.0*(knots_w[i+1]-knots_w[i])/(n+1)*(j+1)
+                                volume.InsertKnot(2, newKnot);
 
 
 def BoundaryLayerCurve(curve, start, scale, n):
@@ -105,105 +105,105 @@ def BoundaryLayerCurve(curve, start, scale, n):
 
 # Geometric distribution of knots
 def GeometricRefineSurface(surface, direction, alpha, n):
-	"""Refine a surface by making a geometric distribution of element sizes
-	Consider Surface.FlipParametrization if you need refinement towards the other edge
-	@param surface: The surface to refine
-	@type surface: Surface 
-	@param direction: The direction to refine in (u=1 or v=2) 
-	@type direction: int
-	@param alpha: The ratio between two sequential knot segments
-	@type alpha: float
-	@param n: The number of knots to insert
-	@type n: int
-	@return: None
-	"""
-	
-	# some error tests on input
-	if n<=0:
-		print 'n should be greater than 0'
-		return None
+        """Refine a surface by making a geometric distribution of element sizes
+        Consider Surface.FlipParametrization if you need refinement towards the other edge
+        @param surface: The surface to refine
+        @type surface: Surface 
+        @param direction: The direction to refine in (u=1 or v=2) 
+        @type direction: int
+        @param alpha: The ratio between two sequential knot segments
+        @type alpha: float
+        @param n: The number of knots to insert
+        @type n: int
+        @return: None
+        """
+        
+        # some error tests on input
+        if n<=0:
+                print 'n should be greater than 0'
+                return None
 
-	flipBack = False
-	if direction < 0:
-		surface.FlipParametrization(-direction-1)
-		direction = -direction
-		flipBack = True
+        flipBack = False
+        if direction < 0:
+                surface.FlipParametrization(-direction-1)
+                direction = -direction
+                flipBack = True
 
-	# fetch knots
-	knots_u, knots_v = surface.GetKnots()
-	if direction == 1:
-		knotStart = knots_u[0]
-		knotEnd   = knots_u[-1]
-	elif direction == 2:
-		knotStart = knots_v[0]
-		knotEnd   = knots_v[-1]
-	else:
-		print 'Direction should be 1 or 2'
-		return None
-	dk = knotEnd - knotStart
+        # fetch knots
+        knots_u, knots_v = surface.GetKnots()
+        if direction == 1:
+                knotStart = knots_u[0]
+                knotEnd   = knots_u[-1]
+        elif direction == 2:
+                knotStart = knots_v[0]
+                knotEnd   = knots_v[-1]
+        else:
+                print 'Direction should be 1 or 2'
+                return None
+        dk = knotEnd - knotStart
 
-	# evaluate the factors
-	n = n+1 # redefine n to be knot spans instead of new internal knots
-	totProd = 1.0
-	totSum  = 0.0
-	for i in range(n):
-		totSum  += totProd
-		totProd *= alpha
-	d1 = 1.0 / totSum
-	knot = d1
+        # evaluate the factors
+        n = n+1 # redefine n to be knot spans instead of new internal knots
+        totProd = 1.0
+        totSum  = 0.0
+        for i in range(n):
+                totSum  += totProd
+                totProd *= alpha
+        d1 = 1.0 / totSum
+        knot = d1
 
-	# do the actual knot insertion
-	for i in range(n-1):
-		surface.InsertKnot(direction-1, knotStart + knot*dk)
-		knot += alpha*d1
-		d1   *= alpha
+        # do the actual knot insertion
+        for i in range(n-1):
+                surface.InsertKnot(direction-1, knotStart + knot*dk)
+                knot += alpha*d1
+                d1   *= alpha
 
-	if flipBack:
-		surface.FlipParametrization(direction-1)
+        if flipBack:
+                surface.FlipParametrization(direction-1)
 
 
 # Edge refinement
 def EdgeRefineSurface(surface, direction, S, n):
-	"""Refine a surface by both edges, by sampling a atan-function
-	@param surface: The surface to refine
-	@type surface: Surface 
-	@param direction: The direction to refine in (u=1 or v=2) 
-	@type direction: int
-	@param S: The slope of the atan-function
-	@type S: float
-	@param n: The number of knots to insert
-	@type n: int
-	@return: None
-	"""
-	
-	# some error tests on input
-	if n<=0:
-		print 'n should be greater than 0'
-		return None
+        """Refine a surface by both edges, by sampling a atan-function
+        @param surface: The surface to refine
+        @type surface: Surface 
+        @param direction: The direction to refine in (u=1 or v=2) 
+        @type direction: int
+        @param S: The slope of the atan-function
+        @type S: float
+        @param n: The number of knots to insert
+        @type n: int
+        @return: None
+        """
+        
+        # some error tests on input
+        if n<=0:
+                print 'n should be greater than 0'
+                return None
 
-	# fetch knots
-	knots_u, knots_v = surface.GetKnots()
-	if direction == 1:
-		knotStart = knots_u[0]
-		knotEnd   = knots_u[-1]
-	elif direction == 2:
-		knotStart = knots_v[0]
-		knotEnd   = knots_v[-1]
-	else:
-		print 'Direction should be 1 or 2'
-		return None
-	dk = knotEnd - knotStart
+        # fetch knots
+        knots_u, knots_v = surface.GetKnots()
+        if direction == 1:
+                knotStart = knots_u[0]
+                knotEnd   = knots_u[-1]
+        elif direction == 2:
+                knotStart = knots_v[0]
+                knotEnd   = knots_v[-1]
+        else:
+                print 'Direction should be 1 or 2'
+                return None
+        dk = knotEnd - knotStart
 
-	# evaluate the factors
-	newKnots = []
-	maxAtan  = atan(S)
-	for i in range(1,n+1):
-		xi  = -1.0 + 2.0*i/(n+1)
-		xi *= S
-		newKnots.append(knotStart + (atan(xi)+maxAtan)/2/maxAtan*dk)
+        # evaluate the factors
+        newKnots = []
+        maxAtan  = atan(S)
+        for i in range(1,n+1):
+                xi  = -1.0 + 2.0*i/(n+1)
+                xi *= S
+                newKnots.append(knotStart + (atan(xi)+maxAtan)/2/maxAtan*dk)
 
-	# do the actual knot insertion
-	for x in newKnots:
-		surface.InsertKnot(direction-1, x)
+        # do the actual knot insertion
+        for x in newKnots:
+                surface.InsertKnot(direction-1, x)
 
 
