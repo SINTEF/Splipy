64db01f90cbcb79dda72dc842b223a811921ae86
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2014-05-07 12:01:32 +0200

    changed: doxy and whitespace fixes for walldistance module

--- GeoUtils/WallDistance.py
+++ GeoUtils/WallDistance.py
@@ -1,69 +1,54 @@
-from GoTools import * 
+from GoTools import *
 import os
 import numpy as np
 import math
 
-def distanceFunction(surfaces, wallset, patches=[]):
+def distanceFunction(surfaces, wallset, patches=[], algorithm='quad+newton'):
+    """Calculate shortest wall distance
+       @param surfaces: Surfaces in model
+       @type surfaces: List of Surface
+       @param wallset: The edges defining the wall
+       @type wallset: List of tuple of (patch, edge) numbers
+       @param: Optional list of patches to process
+       @type patches: List of integer
+       @return: Coefficients of distance field
+       @rtype: List of list of float
     """
-    Calculate shortest wall distance
-        patches = list of all patches for which the wall distance is to be calculated
-        wallsrfid = info about wall (see InputFile.GetTopologSet)
-    """
-    
+
     worksurfaces = []
     if len(patches):
       for patch in patches:
           worksurfaces.append(surfaces[patch-1])
     else:
       worksurfaces = surfaces
-    
+
     wallsurfaces = []
     for idx in wallset:
         wallsurfaces.append(surfaces[idx-1])
-    
+
     wallcurves = []
-    
+
     for idx in wallset:
       for edge in wallset[idx].edge:
-        wallcurves.append(getWallCurve(surfaces[idx-1], edge-1))
+        wallcurves.append(surfaces[idx-1].GetEdges()[edge-1])
 
-    D4 = calcDistQuadMinNewton(wallcurves, worksurfaces)
-    
-    return D4
-    
-def calcDistStraightLine(worksurfaces):
-    """
-    Calculate minimum wall distance based on straight line approximation (assumes perpendicular grid lines)
-    """
-    
-    D = []
-    
-    for surface in worksurfaces:
-        wallcurve = getWallCurve(surface)
-        (knots_xi, knots_eta) = surface.GetKnots()
-        
-        wdist = np.zeros((len(knots_xi), len(knots_eta)))
-        
-        wcrvpts = getCurvePoints(surface, knots_xi, knots_eta[0])
-        
-        j = 0
-        
-        for knot_eta in knots_eta:
-            crvpts = getCurvePoints(surface, knots_xi, knot_eta)
-            i = 0
-            for (wallpt, pt) in zip(wcrvpts, crvpts):
-                wdist[i,j] = calcPtsDistance(wallpt, pt)
-                i = i + 1
-            j = j+1
-        D.append(wdist)
-    
-    return D
+    if algorithm == 'quad+newton':
+      return calcDistQuadMinNewton(wallcurves, worksurfaces)
+    if algorithm == 'quad':
+      return calcDistQuadMin(wallcurves, worksurfaces)
+    if algorithm == 'newton':
+        return calcDistMinNewton(wallcurves, worksurfaces)
 
-
+    return None
 
 def calcDistQuadMinNewton(wallcurves, worksurfaces):
-    """
-    Calculate minimum wall distance based on a combination of quadratic minimization and Newton's method
+    """Calculate minimum wall distance based on a combination of quadratic minimization and Newton's method
+       @param wallcurves: Curves of the wall
+       @type wallcurves: List of Curve
+       @param worksurfaces: Surfaces of the model
+       @type worksurfaces: List of Surface
+       @return: Coefficients of distance field
+       @rtype: List of list of float
     """
     
     D = []
@@ -75,13 +60,13 @@ def calcDistQuadMinNewton(wallcurves, worksurfaces):
         
         (knots_xi, knots_eta) = surface.GetKnots()
         
-        wdist = np.zeros((len(knots_xi), len(knots_eta)))
         s = np.zeros(4)
         curveID = 0
         
         for curve in wallcurves:
             print 'Calculating shortest wall distance from wallcurve ' + str(curveID+1)
             crv_knots_xi = curve.GetKnots()
+            wdist = np.zeros((len(knots_eta), len(knots_xi)))
             
             i = 0
             
@@ -93,7 +78,6 @@ def calcDistQuadMinNewton(wallcurves, worksurfaces):
             for knot_xi in knots_xi:
                 j = 0
                 for knot_eta in knots_eta:
-                    
                     pt = surface.Evaluate(knot_xi, knot_eta)
                     k = 0
 
@@ -113,7 +97,7 @@ def calcDistQuadMinNewton(wallcurves, worksurfaces):
 
                         tmp = s[0]
                     
-                        s_star = calcSingleNewton(curve, crv_knots_xi, tmp, pt) 
+                        s_star = calcSingleNewton(curve, tmp, pt) 
 
                         tol = np.absolute(s_star - tmp_Newton)
                         tmp_Newton = s_star
@@ -122,9 +106,9 @@ def calcDistQuadMinNewton(wallcurves, worksurfaces):
                         tolerance = calcTolerance(crv_knots_xi, tmp_Newton, 1e-10)
 
                         if tol < tolerance:
-                            wdist[i,j] = calcPtsDistance(curve.Evaluate(tmp_Newton), pt)
+                            wdist[j,i] = calcPtsDistance(curve.Evaluate(tmp_Newton), pt)
                             j = j + 1
-                            break;
+                            break
                             
                         if k > 100:  # Fix slow or no convergence
                             tmp = tmp*1.1
@@ -140,8 +124,13 @@ def calcDistQuadMinNewton(wallcurves, worksurfaces):
     return D
     
 def calcDistQuadMin(wallcurves, worksurfaces):
-    """
-    Calculate minimum wall distance based on quadratic minimization
+    """Calculate minimum wall distance based on quadratic minimization
+       @param wallcurves: Curves of the wall
+       @type wallcurves: List of Curve
+       @param worksurfaces: Surfaces of the model
+       @type worksurfaces: List of Surface
+       @return: Coefficients of distance field
+       @rtype: List of list of float
     """
     
     D = []
@@ -187,7 +176,7 @@ def calcDistQuadMin(wallcurves, worksurfaces):
                         
                         if tol < tolerance:
                             wdist[i,j] = calcPtsDistance(curve.Evaluate(s[3]), pt)
-                            #print 'Converged ' + str(i) + ',  ' + str(j) 
+                            print 'Converged ' + str(i) + ',  ' + str(j) 
                             j = j + 1
                             break;
                         
@@ -203,21 +192,22 @@ def calcDistQuadMin(wallcurves, worksurfaces):
         D.append(determineShortest(D_surface))
     return D
 
-
 def calcDistMinNewton(wallcurves, worksurfaces):
+    """Calculate minimum wall distance based on Newton's method
+       @param wallcurves: Curves of the wall
+       @type wallcurves: List of Curve
+       @param worksurfaces: Surfaces of the model
+       @type worksurfaces: List of Surface
+       @return: Coefficients of distance field
+       @rtype: List of list of float
     """
-    Calculate minimum wall distance based on Newton's method
-    """
-    
     
     D = []
-    
     for surface in worksurfaces:
         D_surface = []
         
         (knots_xi, knots_eta) = surface.GetKnots()
         
-        wdist = np.zeros((len(knots_xi), len(knots_eta)))
         s = np.zeros(4)
         
         curveID = 0
@@ -226,7 +216,8 @@ def calcDistMinNewton(wallcurves, worksurfaces):
             crv_knots_xi = curve.GetKnots()
             
             i = 0
-            print 'Calculating shortest wall distance for wallcurve ' + str(curveID)
+            print 'Calculating shortest wall distance for wallcurve ' + str(curveID+1)
+            wdist = np.zeros((len(knots_xi), len(knots_eta)))
             
             length = len(crv_knots_xi)
             
@@ -240,15 +231,14 @@ def calcDistMinNewton(wallcurves, worksurfaces):
                     k = 0
     
                     while True:
-                        s_star = calcSingleNewton(curve, crv_knots_xi, tmp, pt) 
+                        s_star = calcSingleNewton(curve, tmp, pt) 
 
                         tol = np.absolute(s_star - tmp)
                         tmp = s_star
                         
                         tolerance = calcTolerance(crv_knots_xi, tmp, 1e-10)
                         if tol < tolerance:
-                            print str(i) + ', ' + str(j) + ' converged!'
-                            wdist[i,j] = calcPtsDistance(curve.Evaluate(tmp), pt)
+                            wdist[j,i] = calcPtsDistance(curve.Evaluate(tmp), pt)
                             j = j + 1
                             break;
                             
@@ -263,13 +253,17 @@ def calcDistMinNewton(wallcurves, worksurfaces):
         D.append(determineShortestDistance(D_surface))
     return D
 
+def calcSingleNewton(curve, s, pt):
+    """Do a single Newton iteration
+       @param curve: Curve to find closest point on
+       @type curve: Curve
+       @param s: Current parameter estimate
+       @type s: Float
+       @param pt: Point being processed
+       @type pt: Point
+    """
 
-def calcSingleNewton(curve,knots,s, pt):
-    """
-    Do a single Newton iteration
-    """
-    
-    (pt_x, pt_y) = parsePoint(pt)
+    (pt_x, pt_y) = (pt[0], pt[1])
 
     width = 0.0001
     
@@ -282,38 +276,26 @@ def calcSingleNewton(curve,knots,s, pt):
 
     s_star = s - D_p/D_pp
 
-    return limitS(s_star, knots[0], knots[-1])
+    return max(curve.GetKnots()[0], min(s_star, curve.GetKnots()[-1]))
 
-    
-
-def determineSs(wallcurve, pt, s,flag=False):
+def determineSs(wallcurve, pt, s):
     P = np.zeros(4)
 
-    for i in xrange(0,4):
+    for i in range(4):
         P[i] = calcDistPtParam(wallcurve, pt, s[i])
 
     idx = P.argmax()
 
     s[idx] = np.infty
     s.sort()
-    
+
     return s
 
-def limitS(s, smin, smax):
-
-    if s < smin:
-        s = smin
-    elif s > smax:
-        s = smax
-    
-    return s
-    
 def calcTolerance(knots, s_star, tolerance):
-    
     idx = (np.abs(np.array(knots)-s_star)).argmin()
-    
+
     tol = np.infty
-    
+
     if knots[idx] <= s_star:
         if idx == len(knots)-1:
             tol = abs(knots[idx] - knots[idx-1])*tolerance
@@ -324,69 +306,58 @@ def calcTolerance(knots, s_star, tolerance):
             tol = abs(knots[idx+1] - knots[idx])*tolerance
         else:
             tol = abs(knots[idx] - knots[idx-1])*tolerance
-    
+
     return tol
-                    
+
 def calcDistPtParam(wallcurve, pt, s):
-    
-    pt = parsePoint(pt)
-    
-    curvept = parsePoint(wallcurve.Evaluate(s))
-    
+    curvept = wallcurve.Evaluate(s)
+
     return (curvept[0] - pt[0])**2 + (curvept[1] - pt[1])**2
-    
 
 def calcQuadMinParam(wallcurve, pt, s1, s2, s3, s_star_last):
     y12 = s1**2 - s2**2; y23 = s2**2 - s3**2; y31 = s3**2 - s1**2
     s12 = s1 - s2; s23 = s2 - s3; s31 = s3 - s1
-    
+
     D1 = calcDistPtParam(wallcurve, pt, s1)**2
     D2 = calcDistPtParam(wallcurve, pt, s2)**2
     D3 = calcDistPtParam(wallcurve, pt, s3)**2
 
-    
     if abs(s23*D1 + s31*D2 + s12*D3) > 1e-20:
         s_star = 1.0/2* (y23*D1 + y31*D2 + y12*D3) / (s23*D1 + s31*D2 + s12*D3)
     else:
         s_star = np.mean([s1, s2, s3])
-    
-    knots = wallcurve.GetKnots()
-    
-    return limitS(s_star, knots[0], knots[-1])
 
-def getWallCurve(surface, idx):
-    edges = surface.GetEdges()
-    return edges[idx]
-    
-def parsePoint(point):
-    #3D point: 0.254421 -0.0743915 0
-    strpoint = str(point).split()
-    
-    return (float(strpoint[2]), float(strpoint[3]))
-    
-def getCurvePoints(surface,knots_xi, knot_eta):
-    crvpoints = []
-    for knot_xi in knots_xi:
-        crvpoints.append(parsePoint(surface.Evaluate(knot_xi, knot_eta)))
-    
-    return np.array(crvpoints)
-        
+    knots = wallcurve.GetKnots()
+
+    return max(knots[0], min(s_star, knots[-1]))
+
 def calcPtsDistance(pt1, pt2):
-    """
-    Calculate shortest distance between two points
+    """Calculate shortest distance between two points
+       @param pt1: First point
+       @type pt1: Point
+       @param pt2: Second point
+       @type pt2: Point
+       @return: Distance
+       @rtype: Float
     """
     return np.sqrt((pt2[0]-pt1[0])**2 + (pt2[1]-pt1[1])**2)
 
 def determineShortestDistance(Ds):
+    """Determine the shortest distance to the wall, based on arrays of wall distances
+       @param Ds: Distance to the curves
+       @type Ds: List of list of float
+       @return: Minimum distance for each control point
+       @rtype: List of float
     """
-    Determine which array gives the shortes distance to the wall, based on sum of wall distances
-    """
-    
+
     dist = []
 
-    for D in Ds:
-        dist.append(np.sum(D))
-    
-    idx = np.array(dist).argmin()
-    
-    return Ds[idx]
+    for j in range(len(Ds[0])):
+      for k in range(len(Ds[0][j])):
+        dist2 = []
+        for i in range(len(Ds)):
+          dist2.append(Ds[i][j][k])
+        idx = np.array(dist2).argmin()
+        dist.append(dist2[idx])
+
+    return dist
