4e57d6ef238c66483324662102f663c1744f4069
Eivind Fonn <evfonn@gmail.com>
2015-03-03 11:28:27 +0100

    Clean up walldistance output
    - Print on one line
    - Provide an option to turn off output
    
    Scipy warns that `xtol` is an unknown solver option.  It's apparently `xatol`.

--- GeoUtils/WallDistance.py
+++ GeoUtils/WallDistance.py
@@ -1,11 +1,20 @@
 from GoTools import *
 import numpy as np
-import math
+import math, sys
 from scipy.optimize import minimize_scalar
 from scipy.optimize import minimize
 
 # Parallel code copied from the nutils.org project.
 import os, multiprocessing, time
+
+def progress(title, i, tot):
+    args = (title, i, tot, 100 * float(i) / tot)
+    s = '\r%s: %i/%i (%.2f%%)' % args
+    if i == tot:
+        s += '\n'
+    sys.stdout.write(s)
+    sys.stdout.flush()
+
 class Fork( object ):
   'nested fork context, unwinds at exit'
 
@@ -87,7 +96,7 @@ def shzeros( shape, dtype=float ):
   buf = multiprocessing.RawArray( typecode, size )
   return np.frombuffer( buf, dtype ).reshape( shape )
 
-def distanceFunction(patches, wallset, process=[]):
+def distanceFunction(patches, wallset, process=[], display=True):
     """Calculate shortest wall distance
        @param surfaces: Patches in model
        @type surfaces: List of Surface or Volume
@@ -95,19 +104,21 @@ def distanceFunction(patches, wallset, process=[]):
        @type wallset: List of tuple of (patch, edge/face) numbers
        @param process: Optional list of patches to process
        @type process: List of integer
+       @param display: (Optional) Write progress to stdout
+       @type display: Boolean
        @return: Coefficients of distance field
        @rtype: List of list of float
     """
 
     if isinstance(patches[0], Surface):
-      return _distanceFunction2D(patches, wallset, process)
+      return _distanceFunction2D(patches, wallset, process, display=display)
 
     if isinstance(patches[0], Volume):
-      return _distanceFunction3D(patches, wallset, process)
+      return _distanceFunction3D(patches, wallset, process, display=display)
 
     raise ValueError('Can only process surfaces or volumes')
 
-def _distanceFunction2D(surfaces, wallset, patches=[]):
+def _distanceFunction2D(surfaces, wallset, patches=[], display=True):
     """Calculate shortest wall distance in 2D
        @param surfaces: Surfaces in model
        @type surfaces: List of Surface
@@ -115,6 +126,8 @@ def _distanceFunction2D(surfaces, wallset, patches=[]):
        @type wallset: List of tuple of (patch, edge) numbers
        @param: Optional list of patches to process
        @type patches: List of integer
+       @param display: (Optional) Write progress to stdout
+       @type display: Boolean
        @return: Coefficients of distance field
        @rtype: List of list of float
     """
@@ -131,15 +144,17 @@ def _distanceFunction2D(surfaces, wallset, patches=[]):
       for edge in info.edge:
         walledges.append( surfaces[idx-1].GetEdges()[edge-1] )
 
-    D = _calcDistScipy(walledges, worksurfaces)
+    D = _calcDistScipy(walledges, worksurfaces, display=display)
     return D
 
-def _calcDistScipy(wallcurves, worksurfaces):
+def _calcDistScipy(wallcurves, worksurfaces, display=True):
     """Calculate minimum wall distance using scipy and minimize scalar
        @param wallcurves: List of curves describing the wall
        @type wallcurves: List of curve
        @param worksurfaces: Surfaces to process
        @type worksurfaces: List of Surface
+       @param display: (Optional) Write progress to stdout
+       @type display: Boolean
        @return Wall distance
        @rtype List of doubles
     """
@@ -150,7 +165,8 @@ def _calcDistScipy(wallcurves, worksurfaces):
     lenD = shzeros( len(worksurfaces) )
     t0 = time.time()
     for srfID, surface in pariter(enumerate(worksurfaces), GetProcessorCount()):
-        print 'Working on surface number %i/%i of len %i' % (srfID+1, len(D), len(surface))
+        if display:
+            progress('Computing wall distance', srfID+1, len(D))
 
         (knots_xi, knots_eta) = surface.GetGreville()
         wdist = np.zeros((len(knots_xi), len(knots_eta)))
@@ -169,7 +185,7 @@ def _calcDistScipy(wallcurves, worksurfaces):
                     s0 = (crv_knots_xi[-1] + crv_knots_xi[0])/2.0
                     lb = crv_knots_xi[0]
                     ub = crv_knots_xi[-1]
-                    res = minimize_scalar(_calcPtsDistanceCurve, s0, args=(curve, pt), bounds=(lb,ub), method='bounded', options={'xtol': 1e-10, 'disp': False})
+                    res = minimize_scalar(_calcPtsDistanceCurve, s0, args=(curve, pt), bounds=(lb,ub), method='bounded', options={'xtol': 1e-10, 'xatol': 1e-10, 'disp': False})
                     tmp = _calcPtsDistance(curve.Evaluate(res.x), pt)
                     if tmp < mindist:
                         mindist = tmp
@@ -181,7 +197,6 @@ def _calcDistScipy(wallcurves, worksurfaces):
         D[srfID,:size] = data
         lenD[srfID] = size
 
-    print 'elapsed: ', time.time()-t0
     return [list(Di[:lenDi]) for (Di,lenDi) in zip(D,lenD)]
 
 
@@ -219,7 +234,7 @@ def _calcPtsDistanceSurface(s, surface, pt2):
     pt1 = surface.Evaluate(s[0], s[1])
     return np.sqrt((pt2[0]-pt1[0])**2 + (pt2[1]-pt1[1])**2 + (pt2[2]-pt1[2])**2)
 
-def _distanceFunction3D(volumes, wallset, patches=[]):
+def _distanceFunction3D(volumes, wallset, patches=[], display=True):
     """Calculate shortest wall distance in 3D
        @param volumes: Volumes in model
        @type volumes: List of Volume
@@ -227,6 +242,8 @@ def _distanceFunction3D(volumes, wallset, patches=[]):
        @type wallset: List of tuple of (patch, face) numbers
        @param: Optional list of patches to process
        @type patches: List of integer
+       @param display: (Optional) Write progress to stdout
+       @type display: Boolean
        @return: Coefficients of distance field
        @rtype: List of list of float
     """
@@ -243,17 +260,19 @@ def _distanceFunction3D(volumes, wallset, patches=[]):
       for face in info.face:
         wallfaces.append( volumes[idx-1].GetFaces()[face-1] )
 
-    D = _calcDistScipy3D(wallfaces, workvolumes)
+    D = _calcDistScipy3D(wallfaces, workvolumes, display=display)
 
     return D
 
 
-def _calcDistScipy3D(wallfaces, workvolumes):
+def _calcDistScipy3D(wallfaces, workvolumes, display=True):
     """Calculate minimum wall distance using scipy and minimize scalar
        @param wallcurves: List of faces describing the wall
        @type wallcurves: List of Surface
        @param workvolumes: Volumes to process
        @type workvolumes: List of Volume
+       @param display: (Optional) Write progress to stdout
+       @type display: Boolean
        @return Wall distance
        @rtype List of doubles
     """
@@ -264,7 +283,8 @@ def _calcDistScipy3D(wallfaces, workvolumes):
     lenD = shzeros( len(workvolumes) )
     t0 = time.time()
     for volID, volume in pariter( enumerate(workvolumes),GetProcessorCount()):
-        print 'Working on volume number %s/%i of len %i' % (str(volID+1), len(D), len(volume))
+        if display:
+            progress('Computing wall distance', volID+1, len(D))
         (knots_xi, knots_eta, knots_gamma) = volume.GetGreville()
         wdist = np.zeros((len(knots_xi), len(knots_eta), len(knots_gamma)))
 
@@ -297,5 +317,4 @@ def _calcDistScipy3D(wallfaces, workvolumes):
         data = volume.Interpolate(list(wdist.flatten('F')))
         D[volID,:size] = data
         lenD[volID] = size
-    print 'elapsed: ', time.time()-t0
     return [list(Di[:lenDi]) for (Di,lenDi) in zip(D,lenD)]
