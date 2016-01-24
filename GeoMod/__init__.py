94423e051ad87a9038b2ed4585e51c56fc5d46ab
Eivind Fonn <evfonn@gmail.com>
2016-01-24 15:33:41 +0100

    Add package __init__ file

---
+++ GeoMod/__init__.py
@@ -0,0 +1,6 @@
+from GeoMod.BSplineBasis import BSplineBasis
+from GeoMod.Curve import Curve
+from GeoMod.Surface import Surface
+from GeoMod.Volume import Volume
+
+__all__ = ['BSplineBasis', 'Curve', 'Surface', 'Volume']
