445d855efc6ab156d98ccebd903341945ad47e5c
Eivind Fonn <evfonn@gmail.com>
2016-02-11 20:24:46 +0100

    Implement Orientation

--- GeoMod/SplineModel.py
+++ GeoMod/SplineModel.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 
 from GeoMod import *
+from GeoMod.Utils import *
 import numpy as np
+from collections import Counter
+from itertools import product, permutations
 
 try:
     from collections.abc import MutableMapping
@@ -10,22 +13,44 @@ except ImportError:
 
 
 class VertexDict(MutableMapping):
+    """A dictionary where the keys are numpy arrays, and where equality
+    computed in an a pproximate sense for floating point numbers.
+
+    All keys must have the same dimensions.
+    """
 
     def __init__(self, rtol=0, atol=1e-8):
+        """Initialize a vertex dictionary.
+
+        :param float rtol: Relative tolerance to use for equality check
+        :param float atol: Absolute tolerance to use for equality check
+        """
         self.rtol = rtol
         self.atol = atol
+
+        # List of (key, value) pairs
         self.internal = []
 
     def _eq(self, a, b):
+        """Check whether two numpy arrays are almost equal, according to the given
+        tolerances.
+        """
         return np.allclose(a, b, rtol=self.rtol, atol=self.atol)
 
     def _candidate(self, key):
+        """Return the internal index for the first stored mapping that matches the
+        given key.
+
+        :param numpy.array key: The key to look for
+        :raises KeyError: If the key is not found
+        """
         for i, (k, v) in enumerate(self.internal):
             if self._eq(key, k):
                 return i
         raise KeyError(key)
 
     def __setitem__(self, key, value):
+        """Assign a key to a value."""
         try:
             c = self._candidate(key)
             k, _ = self.internal[c]
@@ -34,19 +59,183 @@ class VertexDict(MutableMapping):
             self.internal.append((key, value))
 
     def __getitem__(self, key):
+        """Gets the value assigned to a key.
+
+        :raises KeyError: If the key is not found
+        """
         c = self._candidate(key)
         return self.internal[c][1]
 
     def __delitem__(self, key):
+        """Deletes an assignment."""
         self.internal = [(k, v) for k, v in self.internal
                          if not self._eq(key, k)]
 
     def __iter__(self):
+        """Iterate over all keys.
+
+        .. note:: This generates all the stored keys, not all matching keys.
+        """
         for k, _ in self.internal:
             yield k
 
     def items(self):
+        """Return a list of key, value pairs."""
         return list(self.internal)
 
     def __len__(self):
+        """Returns the number of stored assignments."""
         return len(self.internal)
+
+
+class OrientationError(RuntimeError):
+    """An `OrientationError` is raised by certain methods in
+    :class:`GeoMod.SplineModel.Orientation` indicating an inability to match
+    two objects.
+    """
+    pass
+
+class Orientation(object):
+    """An `Orientation` represents a mapping between two coordinate systems: the
+    *reference* system and the *actual* or *mapped* system.
+
+    The orientation is represented in terms of two tuples:
+
+    - `perm` is a permutation: direction `d` in the reference system is mapped
+      to direction `perm[d]` in the actual system.
+    - `flip` is a tuple of bools: `flip[d]` will be `True` to indicate that
+      direction `d` *in the reference system* should be reversed.
+    """
+
+    def __init__(self, perm, flip):
+        """Initialize an Orientation object.
+
+        This constructor is for internal use. Use
+        :func:`GeoMod.SplineModel.Orientation.compute` to generate new
+        orientation objects.
+        """
+        self.perm = perm
+        self.flip = flip
+        self.perm_inv = tuple(perm.index(d) for d in range(len(perm)))
+
+    @classmethod
+    def compute(cls, cpa, cpb=None):
+        """Compute and return a new orientation object representing the mapping between
+        `cpa` (the reference system) and `cpb` (the mapped system).
+
+        Each argument can be either a `SplineObject` or a numpy array
+        representing the control points.
+
+        If `cpb` is not given, the identity orientation is returned.
+
+        :param cpa: The reference system
+        :param cpb: The mapped system
+        :return: The orientation relating the two systems
+        :rtype: Orientation
+        :raises OrientationError: If the two objects do not match
+        """
+        if not isinstance(cpa, np.ndarray):
+            cpa = cpa.controlpoints
+
+        shape_a = cpa.shape
+        pardim = len(shape_a) - 1
+
+        # Return the identity orientation if no cpb
+        if cpb is None:
+            return cls(tuple(range(pardim)),
+                       tuple(False for _ in range(pardim)))
+
+        if not isinstance(cpb, np.ndarray):
+            cpb = cpb.controlpoints
+
+        shape_b = cpb.shape
+
+        # Deal with the easy cases: dimension mismatch, and
+        # comparing the shapes as multisets
+        if len(shape_a) != len(shape_b):
+            raise OrientationError("Mismatching parametric dimensions")
+        if shape_a[-1] != shape_b[-1]:
+            raise OrientationError("Mismatching physical dimensions")
+        if Counter(shape_a) != Counter(shape_b):
+            raise OrientationError("Non-matching objects")
+
+        # Enumerate all permutations of directions
+        for perm in permutations(range(pardim)):
+            transposed = cpb.transpose(perm + (pardim,))
+            if transposed.shape != shape_a:
+                continue
+            # Enumerate all possible direction reversals
+            for flip in product([False, True], repeat=pardim):
+                slices = tuple(slice(None, None, -1) if f else slice(None) for f in flip)
+                test_b = transposed[slices + (slice(None),)]
+                # FIXME: Hardcoded tolerances
+                if np.allclose(cpa, test_b, rtol=0, atol=1e-8):
+                    return cls(perm, flip)
+
+        raise OrientationError("Non-matching objects")
+
+    @property
+    def pardim(self):
+        return len(self.perm)
+
+    def __mul__(self, other):
+        """Compose two mappings.
+
+        If `ort_left` maps system `A` (reference) to system `B`, and
+        `ort_right` maps system `B` (reference) to system `C`, then `ort_left *
+        ort_right` maps system `A` (reference) to system `C`.
+        """
+        assert self.pardim == other.pardim
+        perm = tuple(other.perm[self.perm[d]] for d in range(self.pardim))
+
+        # Flip a direction if it is flipped in exactly one orientation
+        flip = tuple(self.flip[d] != other.flip[self.perm[d]] for d in range(self.pardim))
+
+        return Orientation(perm, flip)
+
+    def map_section(self, section):
+        """Map a section in the mapped system to the reference system.
+
+        The input is a section tuple as described in
+        :func:`GeoMod.Utils.section`. It should be described relative to the
+        mapped coordinate system. The return value is the corresponding section
+        relative to the reference system.
+        """
+        permuted = tuple(section[d] for d in self.perm)
+
+        flipped = ()
+        for s, f, in zip(permuted, self.flip):
+            # Flipping only applies to indexed directions, not variable ones
+            if f and s is not None:
+                flipped += (0 if s == -1 else -1,)
+            else:
+                flipped += (s,)
+
+        return flipped
+
+    def view_section(self, section):
+        """Reduce a mapping to a lower dimension.
+
+        The input is a section tuple as described in
+        :func:`GeoMod.Utils.section`. It should be described relative to the
+        mapped coordinate system. The return value is an `Orientation` object
+        that describes the same orentation, relative to the section only.
+
+        For example, for a 2D orentation that flips only the second direction,
+        `view_section` will return the identity orientation for edges along the
+        first direction, but the inverse orientation for edges along the second
+        (flipped) direction.
+        """
+        # The directions (in the mapped system) that are variable
+        variable_dirs = [i for i, s in enumerate(section) if s is None]
+
+        # The directions (in the reference system) that are variable
+        # Sort to get the order that they have in the reference system
+        actual_dirs = sorted(self.perm_inv[d] for d in variable_dirs)
+
+        # perm[d] maps a direction to the mapped system, and its index in
+        # variable_dirs reveals the permutation
+        new_perm = tuple(variable_dirs.index(self.perm[d]) for d in actual_dirs)
+
+        new_flip = tuple(self.flip[d] for d in actual_dirs)
+        return self.__class__(new_perm, new_flip)
--- GeoMod/splinemodel_test.py
+++ GeoMod/splinemodel_test.py
@@ -38,3 +38,156 @@ class TestVertexDict(unittest.TestCase):
 
         del d[np.array([0, 0])]
         self.assertEqual(len(d), 0)
+
+
+class TestOrientation(unittest.TestCase):
+
+    def test_curves(self):
+        c1 = Curve()
+        c2 = Curve()
+
+        tmp = Orientation.compute(c1)
+        self.assertEqual(tmp.perm, (0,))
+        self.assertEqual(tmp.flip, (False,))
+
+        tmp = Orientation.compute(c1, c2)
+        self.assertEqual(tmp.perm, (0,))
+        self.assertEqual(tmp.flip, (False,))
+
+        c1.reverse()
+        tmp = Orientation.compute(c1, c2)
+        self.assertEqual(tmp.perm, (0,))
+        self.assertEqual(tmp.flip, (True,))
+
+        c1 += (0.5, 0)
+        with self.assertRaises(OrientationError):
+            Orientation.compute(c1, c2)
+
+        c1 -= (0.5, 0)
+        c1.refine(2)
+        with self.assertRaises(OrientationError):
+            Orientation.compute(c1, c2)
+
+        c2.refine(2)
+        tmp = Orientation.compute(c1.controlpoints, c2.controlpoints)
+        self.assertEqual(tmp.perm, (0,))
+        self.assertEqual(tmp.flip, (True,))
+
+        c2.set_dimension(3)
+        with self.assertRaises(OrientationError):
+            Orientation.compute(c1, c2)
+
+    def test_surfaces(self):
+        c = Curve()
+        s1 = Surface()
+        s2 = Surface()
+
+        s1.refine(1, 0)
+        s2.refine(1, 0)
+
+        with self.assertRaises(OrientationError):
+            Orientation.compute(c, s1)
+
+        tmp = Orientation.compute(s1, s2)
+        self.assertTupleEqual(tmp.perm, (0, 1))
+        self.assertTupleEqual(tmp.flip, (False, False))
+
+        s1.reverse('u')
+        tmp = Orientation.compute(s1, s2)
+        self.assertTupleEqual(tmp.perm, (0, 1))
+        self.assertTupleEqual(tmp.flip, (True, False))
+
+        s2.reverse('v')
+        tmp = Orientation.compute(s1, s2)
+        self.assertTupleEqual(tmp.perm, (0, 1))
+        self.assertTupleEqual(tmp.flip, (True, True))
+
+        s2.reverse('u')
+        tmp = Orientation.compute(s1, s2)
+        self.assertTupleEqual(tmp.perm, (0, 1))
+        self.assertTupleEqual(tmp.flip, (False, True))
+
+        s2.reverse('v')
+        s1.swap()
+        tmp = Orientation.compute(s1, s2)
+        self.assertTupleEqual(tmp.perm, (1, 0))
+        self.assertTupleEqual(tmp.flip, (False, False))
+
+        s1.reverse('u')
+        tmp = Orientation.compute(s1, s2)
+        self.assertTupleEqual(tmp.perm, (1, 0))
+        self.assertTupleEqual(tmp.flip, (True, False))
+
+        s2.reverse('v')
+        tmp = Orientation.compute(s1, s2)
+        self.assertTupleEqual(tmp.perm, (1, 0))
+        self.assertTupleEqual(tmp.flip, (False, False))
+
+        s2.reverse('u')
+        tmp = Orientation.compute(s1, s2)
+        self.assertTupleEqual(tmp.perm, (1, 0))
+        self.assertTupleEqual(tmp.flip, (False, True))
+
+    def test_compose(self):
+        tmp = (Orientation((0, 1), (False, True)) *
+               Orientation((0, 1), (True, True)))
+        self.assertTupleEqual(tmp.perm, (0, 1))
+        self.assertTupleEqual(tmp.flip, (True, False))
+
+        tmp = (Orientation((0, 4, 1, 3, 2), (False, False, False, False, True)) *
+               Orientation((4, 1, 3, 0, 2), (True, False, True, False, False)))
+        self.assertTupleEqual(tmp.perm, (4, 2, 1, 0, 3))
+        self.assertTupleEqual(tmp.flip, (True, False, False, False, False))
+
+    def test_map(self):
+        va = Volume()
+        vb = Volume()
+        vb.swap('u', 'w')
+        vb.swap('u', 'v')
+        vb.reverse('v')
+
+        # va(u,v,w) corresponds to vb(w,u,1-v)
+        ori = Orientation.compute(va, vb)
+
+        self.assertTupleEqual(ori.perm, (2, 0, 1))
+        self.assertTupleEqual(ori.perm_inv, (1, 2, 0))
+        self.assertTupleEqual(ori.flip, (False, False, True))
+
+        sections = [
+            (( 0,  0,  0), ( 0,  0, -1), (), ()),
+            ((-1,  0,  0), ( 0, -1, -1), (), ()),
+            (( 0, -1,  0), ( 0,  0,  0), (), ()),
+            ((-1, -1,  0), ( 0, -1,  0), (), ()),
+            (( 0,  0, -1), (-1,  0, -1), (), ()),
+            ((-1,  0, -1), (-1, -1, -1), (), ()),
+            (( 0, -1, -1), (-1,  0,  0), (), ()),
+            ((-1, -1, -1), (-1, -1,  0), (), ()),
+
+            (( 0,  0, None), (None,  0, -1), (0,), (False,)),
+            ((-1,  0, None), (None, -1, -1), (0,), (False,)),
+            (( 0, -1, None), (None,  0,  0), (0,), (False,)),
+            ((-1, -1, None), (None, -1,  0), (0,), (False,)),
+            (( 0, None,  0), ( 0,  0, None), (0,), (True,)),
+            ((-1, None,  0), ( 0, -1, None), (0,), (True,)),
+            (( 0, None, -1), (-1,  0, None), (0,), (True,)),
+            ((-1, None, -1), (-1, -1, None), (0,), (True,)),
+            ((None,  0,  0), ( 0, None, -1), (0,), (False,)),
+            ((None, -1,  0), ( 0, None,  0), (0,), (False,)),
+            ((None,  0, -1), (-1, None, -1), (0,), (False,)),
+            ((None, -1, -1), (-1, None,  0), (0,), (False,)),
+
+            (( 0, None, None), (None,  0, None), (1, 0), (False, True)),
+            ((-1, None, None), (None, -1, None), (1, 0), (False, True)),
+            ((None,  0, None), (None, None, -1), (1, 0), (False, False)),
+            ((None, -1, None), (None, None,  0), (1, 0), (False, False)),
+            ((None, None,  0), ( 0, None, None), (0, 1), (False, True)),
+            ((None, None, -1), (-1, None, None), (0, 1), (False, True)),
+
+            ((None, None, None), (None, None, None), (2, 0, 1), (False, False, True)),
+        ]
+
+        for sec, mapped_sec, perm, flip in sections:
+            view = ori.view_section(sec)
+            self.assertTupleEqual(view.perm, perm)
+            self.assertTupleEqual(view.flip, flip)
+            self.assertTupleEqual(ori.map_section(sec), mapped_sec)
