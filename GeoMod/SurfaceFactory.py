064eb61fe590cde79126c00bb961bcb87ff29502
Eivind Fonn <evfonn@gmail.com>
2016-02-16 20:49:59 +0100

    Bugfix: x must be a matrix

--- GeoMod/SurfaceFactory.py
+++ GeoMod/SurfaceFactory.py
@@ -261,7 +261,7 @@ def thicken(curve, amount):
         right_points = np.matrix(np.zeros((n, 2)))
         linear = BSplineBasis(2)
 
-        x = curve.evaluate(t)  # curve at interpolation points
+        x = np.matrix(curve.evaluate(t))  # curve at interpolation points
         v = curve.tangent(t)  # velocity at interpolation points
         l = np.sqrt(v[:, 0]**2 + v[:, 1]**2)  # normalizing factor for velocity
         v[:, 0] = v[:, 0] / l
