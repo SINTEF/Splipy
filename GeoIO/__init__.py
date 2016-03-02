a1b65b4015f28290007f3f6a5d8db4b60ed75f94
Kjetil Andre Johannessen <kjetijo@gmail.com>
2016-03-02 12:27:17 +0100

    Added SVG file support

--- GeoIO/__init__.py
+++ GeoIO/__init__.py
@@ -1,6 +1,7 @@
-from HDF5File import *
-from IFEMResultDatabase import *
-from InputFile import *
-from Numberer import *
-from Utilities import *
-from VTUFile import *
+# from HDF5File import *
+# from IFEMResultDatabase import *
+# from InputFile import *
+# from Numberer import *
+# from Utilities import *
+# from VTUFile import *
+from SVG import *
