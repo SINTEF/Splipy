c215f45aa9cd269552eb7759d6994e5c6c8726ed
Kjetil Andre Johannessen <kjetijo@gmail.com>
2016-03-03 13:15:36 +0100

    Removed unsupported IO files

--- GeoIO/__init__.py
+++ GeoIO/__init__.py
@@ -1,9 +1,3 @@
-# from HDF5File import *
-# from IFEMResultDatabase import *
-# from InputFile import *
-# from Numberer import *
-# from Utilities import *
-# from VTUFile import *
 from SVG import *
 from STL import *
 from G2 import *
