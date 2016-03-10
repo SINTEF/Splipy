SINTEF/Splipy

commit 38e936ea931fbb1059ea960199f7432407d3edba

Update documentation

splipy/utils/image.py
@@ -1,26 +1,30 @@
+# -*- coding: utf-8 -*-
+
 __doc__ = 'Implementation of image based mesh generation.'
 
 from splipy import BSplineBasis, curve_factory, surface_factory
 from math import sqrt
-import cv2
 import numpy as np
+import sys
+
+if sys.version_info < (3,):
+    import cv2
 
 def get_corners(X, L=50, R=30, D=15):
-    """CornerDet detects corners of traced outlines using the SAM04 algorithm.
+    """Detects corners of traced outlines using the SAM04 algorithm.
+
     The outline is assumed to constitute a discrete closed curve where each
-    point is included just once. Increasing D and R will give the same number
-    of corners or fewer corners.
-    :param X : A traced outline forming a discrete closed curve
-    :type  X : numpy array of size nx2
-    :param L : Controls the scale at which corners are measured.
-    :type  L : Float
-    :param R : Controls how close corners can appear.
-    :type  R : Float
-    :param D : The lower bound for the corner metric. Corner candidates with
-               lower metric than this are rejected.
-    :type  D : Float
-    :return  : The indices of X that consitute corner points
-    :rtype   : Numpy array
+    point is included just once. Increasing `D` and `R` will give the same
+    number of corners or fewer corners.
+
+    :param numpy.array X: A traced outline forming a discrete closed curve
+        (size *n* × 2)
+    :param float L: Controls the scale at which corners are measured.
+    :param float R: Controls how close corners can appear.
+    :param float D: The lower bound for the corner metric. Corner candidates
+        with lower metric than this are rejected.
+    :return: The indices of X that consitute corner points
+    :rtype: numpy.array
     """
     n = len(X)
 
@@ -95,11 +99,12 @@ def get_corners(X, L=50, R=30, D=15):
 
 
 def image_curves(filename):
-    """Generate B-spline curves corresponding to the edges in a black/white mask image
-    :param filename: Name of image file to read
-    :type  filename: String
+    """Generate B-spline curves corresponding to the edges in a black/white
+    mask image.
+
+    :param str filename: Name of image file to read
     :return: All curves generated by tracing the black/white edges
-    :rtype:  Curve or list of Curves
+    :rtype: [:class:`splipy.Curve`]
     """
     im = cv2.imread(filename)
 
@@ -124,7 +129,7 @@ def image_curves(filename):
         corners = get_corners(pts)
         if len(corners)>2:                        # start/stop tagged as corners. If any inner corners, then
             pts     = np.roll(pts, -corners[1],0) # rearrange points so start/stop falls at a natural corner.
-            corners = get_corners(pts)             # recompute corners, since previous sem might be smooth
+            corners = get_corners(pts)            # recompute corners, since previous sem might be smooth
 
         n = len(pts)
         parpt = range(n)
@@ -170,24 +175,19 @@ def image_curves(filename):
         basis = BSplineBasis(4, knot)
 
         c = curve_factory.least_square_fit(pts, basis, parpt)
-
-        if len(contours) == 2:
-            return c
-        else:
-            result.append(c)
+        result.append(c)
 
     return result
 
 def image_height(filename, N=[30,30], p=[4,4]):
-    """Generate B-spline surface approximation given by the heightmap in a grayscale image
-    :param filename: Name of image file to read
-    :type  filename: String
-    :param N:        Number of controlpoints in u-direction
-    :type  N:        Two Integers
-    :param p:        Polynomial order (degree+1)
-    :type  p:        Two Integers
-    :return:         Normalized (all values between 0 and 1) heightmap approximation
-    :rtype:          Surface
+    """Generate a B-spline surface approximation given by the heightmap in a
+    grayscale image.
+
+    :param str filename: Name of image file to read
+    :param (int,int) N: Number of controlpoints in u-direction
+    :param (int,int) p: Polynomial order (degree+1)
+    :return: Normalized (all values between 0 and 1) heightmap approximation
+    :rtype: :class:`splipy.Surface`
     """
     im = cv2.imread(filename)
 
@@ -224,13 +224,13 @@ def image_height(filename, N=[30,30], p=[4,4]):
 
 def image_convex_surface(filename):
     """Generate a single B-spline surface corresponding to convex black domain
-       of a black/white mask image. The algorithm traces the boundary and
-       searches for 4 natural corner points. It will then generate 4 boundary
-       curves which will be used to create the surface by Coons Patch. 
-    :param filename: Name of image file to read
-    :type  filename: String
-    :return        : B-spline surface
-    :rtype         : Surface
+    of a black/white mask image. The algorithm traces the boundary and searches
+    for 4 natural corner points. It will then generate 4 boundary curves which
+    will be used to create the surface by Coons Patch.
+
+    :param str filename: Name of image file to read
+    :return: B-spline surface
+    :rtype: :class:`splipy.Surface`
     """
     # generate boundary curve
     crv = image_curves(filename);

.gitignore
@@ -6,3 +6,4 @@ doc/_static
 doc/_templates
 /doc/html/
 /doc/latex/
+.dir-locals.el

splipy/curve_factory.py
@@ -7,16 +7,31 @@ from splipy import Curve, BSplineBasis
 from splipy.utils import flip_and_move_plane_geometry
 import numpy as np
 
-__all__ = ['line', 'polygon', 'n_gon', 'circle', 'circle_segment', 'interpolate',
-           'least_square_fit', 'cubic_curve']
+__all__ = ['Boundary', 'line', 'polygon', 'n_gon', 'circle', 'circle_segment',
+           'interpolate', 'least_square_fit', 'cubic_curve']
 
-FREE=1
-NATURAL=2
-HERMITE=3
-PERIODIC=4
-TANGENT=5
-TANGENTNATURAL=6
-UNIFORM=8
+class Boundary:
+    """Enumeration representing different boundary conditions used in
+    :func:`interpolate`."""
+
+    FREE = 1
+    """The curve will be smooth at the second and second-to-last unique knot."""
+
+    NATURAL = 2
+    """The curve will have zero second derivatives at the endpoints."""
+
+    HERMITE = 3
+    """Specify the derivatives at the knots."""
+
+    PERIODIC = 4
+    """The curve will be periodic at the endpoints."""
+
+    TANGENT = 5
+    """Specify the tangents at the endpoints."""
+
+    TANGENTNATURAL = 6
+    """Use `TANGENT` for the start and `NATURAL` for the end."""
+
 
 def line(a, b):
     """Create a line between two points.
@@ -158,13 +173,15 @@ def circle_segment(theta, r=1, center=(0,0,0), normal=(0,0,1)):
     return flip_and_move_plane_geometry(result, center, normal)
 
 def interpolate(x, basis, t=None):
-    """Perform general spline interpolation on a provided basis.
+    """interpolate(x, basis, [t=None])
+
+    Perform general spline interpolation on a provided basis.
 
     :param matrix-like x: Matrix *X[i,j]* of interpolation points *xi* with
         components *j*
     :param BSplineBasis basis: Basis on which to interpolate
-    :param array-like   t    : parametric values at interpolation points, defaults
-                               to greville points if not provided
+    :param array-like t: parametric values at interpolation points; defaults to
+        Greville points if not provided
     :return: Interpolated curve
     :rtype: Curve
     """
@@ -188,7 +205,7 @@ def least_square_fit(x, basis, t):
         components *j*. The number of points must be equal to or larger than
         the number of basis functions in *basis*
     :param BSplineBasis basis: Basis on which to interpolate
-    :param array-like   t    : parametric values at evaluation points
+    :param array-like t: parametric values at evaluation points
     :return: Approximated curve
     :rtype: Curve
     """
@@ -204,17 +221,26 @@ def least_square_fit(x, basis, t):
     return Curve(basis, controlpoints)
 
 
-def cubic_curve(x, boundary=FREE, t=None, tangents=None):
-    """Perform cubic spline interpolation on a provided basis.
+def cubic_curve(x, boundary=Boundary.FREE, t=None, tangents=None):
+    """cubic_curve(x, [boundary=Boundary.FREE], [t=None], [tangents=None])
 
-    :param matrix-like x: Matrix *X[i,j]* of interpolation points *xi* with
+    Perform cubic spline interpolation on a provided basis.
+
+    The valid boundary conditions are enumerated in :class:`Boundary`. The
+    meaning of the `tangents` parameter depends on the specified boundary
+    condition:
+
+    - `TANGENT`: two points,
+    - `TANGENTNATURAL`: one point,
+    - `HERMITE`: *n* points
+
+    :param matrix-like x: Matrix *X[i,j]* of interpolation points *x_i* with
         components *j*
-    :param int boundary: FREE, NATURAL, HERMITE, PERIODIC, TANGENT or TANGENTNATURAL
+    :param int boundary: Any value from :class:`Boundary`.
     :param array-like t: parametric values at interpolation points, defaults
-        to euclidian distance between evaluation points
-    :param matrix-like tangents: In case of HERMITE or TANGENT boundary
-        condtions, one must supply tangent information (two points for TANGENT,
-        n points for HERMITE)
+        to Euclidean distance between evaluation points
+    :param matrix-like tangents: Tangent information according to the boundary
+        conditions.
     :return: Interpolated curve
     :rtype: Curve
     """
@@ -228,14 +254,14 @@ def cubic_curve(x, boundary=FREE, t=None, tangents=None):
 
     # modify knot vector for chosen boundary conditions
     knot = [t[0]]*3 + list(t) + [t[-1]]*3
-    if boundary == FREE:
+    if boundary == Boundary.FREE:
         del knot[-5]
         del knot[4]
-    elif boundary == HERMITE:
+    elif boundary == Boundary.HERMITE:
         knot = sorted(knot + t[1:-1])
 
     # create the interpolation basis and interpolation matrix on this
-    if boundary == PERIODIC:
+    if boundary == Boundary.PERIODIC:
         knot[0]  = t[-3] - t[-1]
         knot[1]  = t[-2] - t[-1]
         knot[-2] = t[-1] + t[1]
@@ -246,17 +272,17 @@ def cubic_curve(x, boundary=FREE, t=None, tangents=None):
     N = basis(t)  # left-hand-side matrix
 
     # add derivative boundary conditions if applicable
-    if boundary==TANGENT or boundary==HERMITE or boundary==TANGENTNATURAL:
-        if boundary == TANGENT:
+    if boundary in [Boundary.TANGENT, Boundary.HERMITE, Boundary.TANGENTNATURAL]:
+        if boundary == Boundary.TANGENT:
             dn = basis([t[0], t[-1]], d=1)
             N  = np.resize(N, (N.shape[0]+2, N.shape[1]))
             x  = np.resize(x, (x.shape[0]+2, x.shape[1]))
             x[n:,:] = tangents 
-        elif boundary == TANGENTNATURAL:
+        elif boundary == Boundary.TANGENTNATURAL:
             dn = basis(t[0], d=1)
             N  = np.resize(N, (N.shape[0]+1, N.shape[1]))
             x  = np.resize(x, (x.shape[0]+1, x.shape[1]))
-        elif boundary == HERMITE:
+        elif boundary == Boundary.HERMITE:
             dn = getBasis(t, d=1)
             N  = np.resize(N, (N.shape[0]+n, N.shape[1]))
             x  = np.resize(x, (x.shape[0]+n, x.shape[1]))
@@ -264,11 +290,11 @@ def cubic_curve(x, boundary=FREE, t=None, tangents=None):
         N[n:,:] = dn
 
     # add double derivative boundary conditions if applicable
-    if boundary == NATURAL or boundary == TANGENTNATURAL:
-        if boundary == NATURAL:
+    if boundary in [Boundary.NATURAL, Boundary.TANGENTNATURAL]:
+        if boundary == Boundary.NATURAL:
             ddn  = basis([t[0], t[-1]], d=2)
             new  = 2
-        elif boundary == TANGENTNATURAL:
+        elif boundary == Boundary.TANGENTNATURAL:
             ddn  = basis(t[-1], d=2)
             new  = 1
         N  = np.resize(N, (N.shape[0]+new, N.shape[1]))

splipy/factory_test.py
@@ -170,7 +170,7 @@ class TestCurveFactory(unittest.TestCase):
         x = np.zeros((80,2))
         x[:,0] = 16*t*t*(1-t)*(1-t)
         x[:,1] = 1-x[:,0]
-        crv = CurveFactory.cubic_curve(x, CurveFactory.PERIODIC, t)
+        crv = CurveFactory.cubic_curve(x, CurveFactory.Boundary.PERIODIC, t)
 
         t = np.linspace(0,1,150)
         x = crv(t)

splipy/io/g2.py
@@ -1,9 +1,10 @@
 import numpy as np
 from itertools import chain, product
 from splipy import BSplineBasis, Curve, Surface, Volume, SplineObject
+from .master import MasterIO
 
 
-class G2(object):
+class G2(MasterIO):
 
     g2_type = [100, 200, 700] # curve, surface, volume identifiers
     classes = [Curve, Surface, Volume]
@@ -80,6 +81,7 @@ class G2(object):
 
             args = bases + [cps, rational]
             result.append(cls(*args))
+
         return result
 
     def read_basis(self):

splipy/io/master.py
@@ -1,0 +1,27 @@
+class MasterIO(object):
+
+    def __init__(self, filename):
+        """Create an IO object attached to a file.
+
+        :param str filename: The file to read from or write to
+        """
+        raise NotImplementedError()
+
+    def __enter__(self):
+        raise NotImplementedError()
+
+    def write(self, obj):
+        """Write one or more objects to the file.
+
+        :param obj: The object(s) to write
+        :type obj: [:class:`splipy.SplineObject`] or :class:`splipy.SplineObject`
+        """
+        raise NotImplementedError()
+
+    def read(self):
+        """Reads all the objects from the file.
+
+        :return: Objects
+        :rtype: [:class:`splipy.SplineObject`]
+        """
+        raise NotImplementedError()

splipy/io/stl.py
@@ -2,8 +2,9 @@
 
 import struct
 import numpy as np
-from splipy.Utils import ensure_listlike
+from splipy.utils import ensure_listlike
 from splipy import SplineModel, Surface, Volume
+from .master import MasterIO
 
 ASCII_FACET = """facet normal 0 0 0
 outer loop
@@ -83,7 +84,7 @@ class BINARY_STL_Writer(ASCII_STL_Writer):
         self.fp.write(struct.pack(BINARY_FACET, *data))
 
 
-class STL(object):
+class STL(MasterIO):
     def __init__(self, filename, binary=True):
         if filename[-4:] != '.stl':
             filename += '.stl'

splipy/io/svg.py
@@ -4,6 +4,7 @@ from splipy import Curve, Surface, SplineObject, BSplineBasis
 import xml.etree.ElementTree as etree
 import numpy as np
 import re
+from .master import MasterIO
 
 def bezier_representation(curve):
     """ Compute a Bezier representation of a given spline curve. The input
@@ -34,7 +35,7 @@ def bezier_representation(curve):
     return bezier
 
 
-class SVG(object):
+class SVG(MasterIO):
     
     namespace = '{http://www.w3.org/2000/svg}'
 
@@ -326,8 +327,3 @@ class SVG(object):
         # invert y-values since these are image coordinates
         result[:,1] = self.height - result[:,1]
         return result
-            
-
-
-
-

commit 687995de9555dda2c91b67d7c2d2fdb925b682a2

38e936ea931fbb1059ea960199f7432407d3edba broke image tests. Fixed now

splipy/utils/image.py
@@ -236,8 +236,10 @@ def image_convex_surface(filename):
     crv = image_curves(filename);
 
     # error test input
-    if type(crv) is list:
+    if len(crv) != 1:
         raise RuntimeError('Error: image_convex_surface expects a single closed curve. Multiple curves detected')
+
+    crv = crv[0]
 
     # parametric value of corner candidates. These are all in the range [0,1] and both 0 and 1 is present
     kinks = crv.get_kinks()

splipy/utils/image_test.py
@@ -14,6 +14,8 @@ class TestImage(unittest.TestCase):
     @unittest.skipIf(sys.version_info >= (3,), "Image module not supported on Python 3")
     def test_curve(self):
         crv = image_curves('splipy/utils/disc.png')
+        self.assertEqual(type(crv), list)
+        crv = crv[0]
         self.assertEqual(type(crv), Curve)
 
         bb = crv.bounding_box()