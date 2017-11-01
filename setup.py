36624040323603059f7b1beccb97b998f58262c1
Kjetil Andre Johannessen <kjetijo@gmail.com>
2017-11-01 13:04:31 +0100

    Updated PyPI meta information to be more comprehensive

--- setup.py
+++ setup.py
@@ -7,6 +7,8 @@ setup(
     name='Splipy',
     version='1.1.0',
     description='Spline modelling library for Python',
+    long_description='This repository contains the Splipy packages. Splipy is a pure python library for the creation, evaluation and manipulation of B-spline and NURBS geometries. It supports n-variate splines of any dimension, but emphasis is made on the use of curves, surfaces and volumes. The library is designed primarily for analysis use, and therefore allows fine-grained control over many aspects which is not possible to achieve with conventional CAD tools.',
+    keywords=['B-spline', 'Splines', 'NURBS', 'Curve', 'Surface', 'Volume', 'Interpolation', 'Approximation', 'Fit', 'Integration', 'Differentiation'],
     url='https://github.com/sintefmath/Splipy',
     maintainer='Kjetil Andre Johannessen',
     maintainer_email='kjetijo@gmail.com',
