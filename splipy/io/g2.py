746b543ff65974d1ff30bbcf5ff1abd14aaac9e7
Kjetil Andre Johannessen <kjetijo@gmail.com>
2018-04-09 09:14:37 +0200

    Set G2 files to double precision

--- splipy/io/g2.py
+++ splipy/io/g2.py
@@ -301,11 +301,11 @@ class G2(MasterIO):
         self.fstream.write('{} {}\n'.format(obj.dimension, int(obj.rational)))
         for b in obj.bases:
             self.fstream.write('%i %i\n' % (len(b.knots) - b.order, b.order))
-            self.fstream.write(' '.join('%f' % k for k in b.knots))
+            self.fstream.write(' '.join('%.16g' % k for k in b.knots))
             self.fstream.write('\n')
 
         for cp in obj:
-            self.fstream.write(' '.join('%f' % x for x in cp))
+            self.fstream.write(' '.join('%.16g' % x for x in cp))
             self.fstream.write('\n')
 
     def read(self):
