a3bd5dd78395a90ec4c6ca01ff5205cafa9a725d
Eivind Fonn <evfonn@gmail.com>
2018-04-18 10:50:04 +0200

    Update push_documentation.py
    Also add instructions in README.rst

--- push_documentation.py
+++ push_documentation.py
@@ -1,29 +1,48 @@
 #!/usr/bin/env python3
 
-from subprocess import run
+from subprocess import run, CalledProcessError
 from tempfile import TemporaryDirectory
 from os.path import abspath, dirname, isdir, join
 from os import listdir, remove
 from shutil import copytree, rmtree
+import sys
 
 
 if __name__ == '__main__':
-    src = abspath(join(dirname(__file__), '..'))
+    if len(sys.argv) > 1:
+        remote = sys.argv[1]
+    else:
+        remote = input('Name of remote to push documentation to: ')
+
+    src = abspath(dirname(__file__))
+
+    # Ensure that local branch gh-pages exists
+    try:
+        run(['git', 'rev-parse', 'gh-pages'], cwd=src, check=True)
+    except CalledProcessError:
+        run(['git', 'branch', 'gh-pages', '{}/gh-pages'.format(remote)], check=True)
+
+    # Ensure that local branch gh-pages is updated
+    run(['git', 'fetch', remote, 'gh-pages:gh-pages'], check=True)
+
     with TemporaryDirectory() as tgt:
-        run(['git', 'clone', src, tgt])
-        run(['git', 'checkout', 'gh-pages'], cwd=tgt)
-        for c in listdir(tgt):
-            if c != '.git':
-                c = join(tgt, c)
-                if isdir(c):
-                    rmtree(c)
-                else:
-                    remove(c)
-        build = join(src, 'docs', 'build', 'html')
+
+        # Clone to a temporary directory and check out the gh-pages branch
+        run(['git', 'clone', src, tgt], check=True)
+        run(['git', 'checkout', 'gh-pages'], cwd=tgt, check=True)
+
+        # Copy files from doc/_build/html over
+        build = join(src, 'doc', '_build', 'html')
         for c in listdir(build):
-            run(['cp', '-R', join(build, c), join(tgt, c)])
-        run(['git', 'add', '-A'], cwd=tgt)
-        run(['git', 'status'], cwd=tgt)
-        run(['git', 'commit', '--allow-empty', '-m', 'Update documentation'], cwd=tgt)
-        run(['git', 'push', 'origin', 'gh-pages'], cwd=tgt)
-        run(['git', 'push', 'origin', 'gh-pages'], cwd=src)
+            run(['cp', '-R', join(build, c), tgt])
+
+        # Add them all, show git status and commit
+        run(['git', 'add', '-A'], cwd=tgt, check=True)
+        run(['git', 'status'], cwd=tgt, check=True)
+        run(['git', 'commit', '--allow-empty', '-m', 'Update documentation'], cwd=tgt, check=True)
+
+        # Push to local repository from the temp repository
+        run(['git', 'push', 'origin', 'gh-pages'], cwd=tgt, check=True)
+
+        # Push to the remote
+        run(['git', 'push', remote, 'gh-pages'], cwd=src, check=True)
