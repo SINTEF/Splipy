344a9df83d5bc4bd66446d5a94a04821bbcd0375
Kjetil Johannessen <kjetijo@gmail.com>
2019-11-28 15:51:18 +0100

    Fixup: test in last pull request was not written correctly

--- test/io/grdecl_test.py
+++ test/io/grdecl_test.py
@@ -10,27 +10,31 @@ THIS_DIR = os.path.dirname(os.path.abspath(__file__))
 class TestGRDECL(unittest.TestCase):
 
     def test_read(self):
-        with GRDECL(THIS_DIR + '/test_geometries/EightCells.grdecl') as myfile:
-            vol1, vol2, vol3 = myfile.read()
-            print()
-            print()
-            print(vol1)
-            print()
-            print()
-            print()
-            print(vol2)
-            print()
-            print()
-            print(vol3)
-            print()
-            print()
+        with GRDECL(THIS_DIR + '/geometries/EightCells.grdecl') as myfile:
+            myfile.read()
+            vol    = myfile.get_c0_mesh() # c0 mesh, compute average value at nodes
+            mx_vol = myfile.get_mixed_cont_mesh() # c0 in (x,y)-directions, C^{-1} in z-direction
+            dc_vol = myfile.get_cm1_mesh() # C^{-1} mesh in all directions
 
-        self.assertEqual(vol1.pardim, 3)
-        self.assertEqual(vol2.pardim, 3)
-        self.assertEqual(vol3.pardim, 3)
-        self.assertEqual(vol1.dimension, 3)
-        # self.assertEqual(vol.order(), (5, 2, 5))
-        # self.assertEqual(vol.shape, (14, 2, 8))
+            # check volume output
+            self.assertEqual(vol.pardim,    3)
+            self.assertEqual(mx_vol.pardim, 3)
+            self.assertEqual(dc_vol.pardim, 3)
+
+            # in three spatial dimensions
+            self.assertEqual(vol.dimension,    3)
+            self.assertEqual(mx_vol.dimension, 3)
+            self.assertEqual(dc_vol.dimension, 3)
+
+            # all linear spline representations
+            self.assertEqual(vol.order(),    (2, 2, 2))
+            self.assertEqual(mx_vol.order(), (2, 2, 2))
+            self.assertEqual(dc_vol.order(), (2, 2, 2))
+
+            # different number of basis functions dending on continuity
+            self.assertEqual(vol.shape,    (3,3,3))
+            self.assertEqual(mx_vol.shape, (3,3,4))
+            self.assertEqual(dc_vol.shape, (4,4,4))
 
 if __name__ == '__main__':
     unittest.main()
