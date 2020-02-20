4b68ea34ba811f72d972fb075bd97148016c73df
Eivind Fonn <evfonn@gmail.com>
2020-02-20 13:23:08 +0100

    grdecl: don't fail if opencv not installed

--- splipy/io/__init__.py
+++ splipy/io/__init__.py
@@ -2,7 +2,12 @@ from .g2  import G2
 from .svg import SVG
 from .spl import SPL
 from .stl import STL
-from .grdecl import GRDECL
 from .ofoam import OpenFOAM
+
+# GRDECL depends on optional cv2
+try:
+    from .grdecl import GRDECL
+except ImportError:
+    pass
 
 __all__ = ['G2', 'SVG', 'SPL', 'STL', 'OpenFOAM', 'GRDECL']
