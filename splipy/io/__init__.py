c9590c7085e54d74f55d5ec4b3d4a24451a856e0
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2021-02-15 13:13:47 +0100

    add 3dm reader

--- splipy/io/__init__.py
+++ splipy/io/__init__.py
@@ -1,4 +1,5 @@
 from .g2  import G2
+from .threedm  import ThreeDM
 from .svg import SVG
 from .spl import SPL
 from .stl import STL
@@ -12,7 +13,7 @@ except ImportError:
     has_grdecl = False
 
 
-__all__ = ['G2', 'SVG', 'SPL', 'STL', 'OpenFOAM']
+__all__ = ['G2', 'SVG', 'SPL', 'STL', 'OpenFOAM', 'Rhino Ceros']
 
 if has_grdecl:
     __all__.append('GRDECL')
---
+++ splipy/io/threedm.py
@@ -0,0 +1,102 @@
+import numpy as np
+from itertools import chain, product
+from splipy import Curve, Surface, Volume, SplineObject, BSplineBasis, TrimmedSurface
+from splipy.utils import flip_and_move_plane_geometry, rotate_local_x_axis
+from .master import MasterIO
+import splipy.surface_factory as SurfaceFactory
+import splipy.curve_factory   as CurveFactory
+import splipy.state as state
+from numpy import sqrt, pi, ndarray
+from rhino3dm import Brep, File3dm, NurbsCurve, NurbsSurface
+
+
+class ThreeDM(MasterIO):
+
+    def __init__(self, filename):
+        if filename[-4:] != '.3dm':
+            filename += '.3dm'
+        self.filename = filename
+        self.trimming_curves = []
+
+    def __enter__(self):
+        return self
+
+    def write(self, obj):
+        raise IOError('Writing to 3DM not supported')
+
+    def read(self):
+        if not hasattr(self, 'fstream'):
+            self.onlywrite = False
+            print('jada %s' %(self.filename))
+            self.fstream   = File3dm.Read(self.filename)
+            print(self.fstream)
+
+        if self.onlywrite:
+            raise IOError('Could not read from file %s' % (self.filename))
+
+        result = []
+
+        for obj in self.fstream.Objects:
+            if type(obj.Geometry) is Brep:
+                for idx in range(0,len(obj.Geometry.Faces)):
+                    nsrf = obj.Geometry.Faces[idx].UnderlyingSurface().ToNurbsSurface()
+                    result.append(self.read_surface(nsrf))
+
+            if type(obj.Geometry) is NurbsCurve:
+                result.append(self.read_curve(obj.Geometry))
+
+            if type(obj.Geometry) is NurbsSurface:
+                result.append(self.read_surface(obj.Geometry))
+
+        return result
+
+    def read_surface(self, nsrf):
+        knotsu = [0]
+        for i in nsrf.KnotsU:
+            knotsu.append(i)
+        knotsu.append(knotsu[len(knotsu)-1])
+        knotsu[0] = knotsu[1]
+
+        knotsv = [0]
+        for i in nsrf.KnotsV:
+            knotsv.append(i)
+        knotsv.append(knotsv[len(knotsv)-1])
+        knotsv[0] = knotsv[1]
+
+        basisu = BSplineBasis(nsrf.OrderU, knotsu, -1)
+        basisv = BSplineBasis(nsrf.OrderV, knotsv, -1)
+        cpts = []
+
+        cpts = ndarray((nsrf.Points.CountU*nsrf.Points.CountV, 3 + nsrf.IsRational))
+        for v in range(0,nsrf.Points.CountV):
+            for u in range(0,nsrf.Points.CountU):
+                cpts[u+v*nsrf.Points.CountU,0] = nsrf.Points[u,v].X
+                cpts[u+v*nsrf.Points.CountU,1] = nsrf.Points[u,v].Y
+                cpts[u+v*nsrf.Points.CountU,2] = nsrf.Points[u,v].Z
+                if nsrf.IsRational:
+                    cpts[u+v*nsrf.Points.CountU,3] = nsrf.Points[u,v].W
+
+        return Surface(basisu, basisv, cpts, nsrf.IsRational)
+
+    def read_curve(self, ncrv):
+        knots = [0]
+        for i in ncrv.Knots:
+            knots.append(i)
+        knots[0] = knots[1]
+        knots.append(knots[len(knots)-1])
+        basis = BSplineBasis(ncrv.Order, knots, -1)
+        cpts = []
+
+        cpts = ndarray((len(ncrv.Points), 3 + obj.Geometry.IsRational))
+        for u in range(0,len(ncrv.Points)):
+            cpts[u,0] = ncrv.Points[u].X
+            cpts[u,1] = ncrv.Points[u].Y
+            cpts[u,2] = ncrv.Points[u].Z
+            if ncrv.IsRational:
+                cpts[u,3] = ncrv.Points[u].W
+
+        return Curve(basis, cpts, ncrv.IsRational)
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        if hasattr(self, 'fstream'):
+            self.fstream.close()
