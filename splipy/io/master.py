9c97ea98672edf54dba42d42f92c4b92ce77ee7a
Eivind Fonn <evfonn@gmail.com>
2024-02-07 15:27:39 +0100

    Type safety: io/master.py

--- splipy/io/master.py
+++ splipy/io/master.py
@@ -1,16 +1,36 @@
-class MasterIO(object):
+from abc import ABC, abstractmethod
+from typing import Optional, Type, Union, Sequence
+from types import TracebackType
 
-    def __init__(self, filename):
-        """Create an IO object attached to a file.
+from typing_extensions import Self
 
-        :param str filename: The file to read from or write to
-        """
-        raise NotImplementedError()
+from ..splineobject import SplineObject
 
-    def __enter__(self):
-        raise NotImplementedError()
 
-    def write(self, obj):
+class MasterIO(ABC):
+
+    # @abstractmethod
+    # def __init__(self, filename):
+    #     """Create an IO object attached to a file.
+
+    #     :param str filename: The file to read from or write to
+    #     """
+    #     raise NotImplementedError()
+
+    @abstractmethod
+    def __enter__(self) -> Self:
+        ...
+
+    @abstractmethod
+    def __exit__(
+        self,
+        exc_type: Optional[Type[BaseException]],
+        exc_val: Optional[BaseException],
+        exc_tb: Optional[TracebackType]
+    ) -> None:
+        ...
+
+    def write(self, obj: Union[SplineObject, Sequence[SplineObject]]):
         """Write one or more objects to the file.
 
         :param obj: The object(s) to write
@@ -18,7 +38,7 @@ class MasterIO(object):
         """
         raise NotImplementedError()
 
-    def read(self):
+    def read(self) -> list[SplineObject]:
         """Reads all the objects from the file.
 
         :return: Objects
