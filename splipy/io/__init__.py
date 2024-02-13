bc6c147b7364707d496ad3f8c38e67266e7d147a
Eivind Fonn <evfonn@gmail.com>
2024-02-13 21:20:44 +0100

    Fix splipy.io optional imports

--- splipy/io/__init__.py
+++ splipy/io/__init__.py
@@ -4,27 +4,23 @@ from .spl import SPL
 from .stl import STL
 from .ofoam import OpenFOAM
 
+__all__ = ['G2', 'SVG', 'SPL', 'STL', 'OpenFOAM']
 
 from importlib.util import find_spec
+
+
 has_cv2 = find_spec('cv2')
-has_rhino = find_spec('rhino3dm')
 
 
 # GRDECL depends on optional cv2
 has_grdecl = has_cv2
-if has_cv2:
-    pass
+if has_grdecl:
+    from .grdecl import GRDECL
+    __all__.append('GRDECL')
 
 
 # ThreeDM depends on optional rhino3dm
+has_rhino = find_spec('rhino3dm')
 if has_rhino:
-    pass
-
-
-__all__ = ['G2', 'SVG', 'SPL', 'STL', 'OpenFOAM']
-
-if has_grdecl:
-    __all__.append('GRDECL')
-
-if has_rhino:
+    from .threedm import ThreeDM
     __all__.append('ThreeDM')
