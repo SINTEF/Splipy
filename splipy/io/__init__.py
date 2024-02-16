5ad0e152bddb2b7b5b6e70f922675e0ee763d879
Eivind Fonn <evfonn@gmail.com>
2024-02-16 17:47:54 +0100

    Fix io module conditional imports

--- splipy/io/__init__.py
+++ splipy/io/__init__.py
@@ -14,10 +14,12 @@ has_cv2 = find_spec("cv2")
 # GRDECL depends on optional cv2
 has_grdecl = has_cv2
 if has_grdecl:
+    from .grdecl import GRDECL  # noqa
     __all__.append("GRDECL")
 
 
 # ThreeDM depends on optional rhino3dm
 has_rhino = find_spec("rhino3dm")
 if has_rhino:
+    from .threedm import ThreeDM  # noqa
     __all__.append("ThreeDM")
